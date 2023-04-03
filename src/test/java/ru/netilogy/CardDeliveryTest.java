326ab1250baddc6b3c23348db9884b1901b39d14
Anastasia test <gorandi@inbox.ru>
2023-04-03 20:21:39 +0300

    add allure

--- src/test/java/ru/netilogy/CardDeliveryTest.java
+++ src/test/java/ru/netilogy/CardDeliveryTest.java
@@ -1,9 +1,9 @@
 package ru.netilogy;
 
 import com.codeborne.selenide.Condition;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.Keys;
 
 import java.time.Duration;
@@ -13,9 +13,20 @@ import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Selenide.$;
 
 public class CardDeliveryTest {
+    @BeforeAll
+    @DisplayName("включение allure")
+    static void setUpAll() {
+//        SelenideLogger.addListener("allure", new AllureSelenide());
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
     @BeforeEach
     void setup() {
         open("http://localhost:9999");
+    }
+    @AfterAll
+    @DisplayName("Выключение allure")
+    public static void tearDownAll() {
+        SelenideLogger.removeListener("allure");
     }
 
 
